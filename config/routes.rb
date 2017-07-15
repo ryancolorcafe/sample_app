74dfd2d0416a4c23b1c9519b28b0cc85f003fb5b
Ryan Brown <ryan.brown@barre3.com>
2017-07-15 06:47:13 -0700

    Finish layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,6 @@ Rails.application.routes.draw do
   get  '/help',    to: 'static_pages#help'
   get  '/about',   to: 'static_pages#about'
   get  '/contact', to: 'static_pages#contact'
+  get  '/signup', to: 'users#new'
+  get  '/login', to: 'static_pages#login'
 end
