d451f7b52b0e8863539ddd177c5c570a7c1c6285
Ryan Brown <browneryan87@gmail.com>
2017-10-23 07:16:40 -0700

    Add Micropost model, validations, tests

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -56,23 +56,31 @@ class UserTest < ActiveSupport::TestCase
   end
 
   test "email addresses should be saved as lower-case" do
-      mixed_case_email = "Foo@ExAMPle.CoM"
-      @user.email = mixed_case_email
-      @user.save
-      assert_equal mixed_case_email.downcase, @user.reload.email
-    end
+    mixed_case_email = "Foo@ExAMPle.CoM"
+    @user.email = mixed_case_email
+    @user.save
+    assert_equal mixed_case_email.downcase, @user.reload.email
+  end
 
-    test "password should be present (nonblank)" do
-      @user.password = @user.password_confirmation = " " * 6
-      assert_not @user.valid?
-    end
+  test "password should be present (nonblank)" do
+    @user.password = @user.password_confirmation = " " * 6
+    assert_not @user.valid?
+  end
 
-    test "password should have a minimum length" do
-      @user.password = @user.password_confirmation = "a" * 5
-      assert_not @user.valid?
-    end
+  test "password should have a minimum length" do
+    @user.password = @user.password_confirmation = "a" * 5
+    assert_not @user.valid?
+  end
 
-    test "authenticated? should return false for a user with nil digest" do
-      assert_not @user.authenticated?(:remember, '')
+  test "authenticated? should return false for a user with nil digest" do
+    assert_not @user.authenticated?(:remember, '')
+  end
+  
+  test "associated microposts should be destroyed" do
+    @user.save
+    @user.microposts.create!(content: "Lorem ipsum")
+    assert_difference 'Micropost.count', -1 do
+      @user.destroy
     end
+  end
 end
